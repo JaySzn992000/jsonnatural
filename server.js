4b2179820dc5bd6728f93152263b4f6710f0d1a5
JaySzn992000 <121809806+JaySzn992000@users.noreply.github.com>
2025-08-24 10:54:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1730,8 +1730,8 @@ setInterval(() => {
 
 
 const razorpayInstance = new Razorpay({
-key_id: "rzp_live_Zm7uF61IDcY0t9", //  Razorpay key_id
-key_secret: "FgZimfWqOEOLs4ejcIZHO7yc", // Razorpay key_secret
+key_id: "rzp_live_HQaw8Iffv44LKF", //  Razorpay key_id
+key_secret: "B8UdFDiIjogxokdY1Nz0yk8R", // Razorpay key_secret
 });
 
 app.post("/create-order", async (req, res) => {

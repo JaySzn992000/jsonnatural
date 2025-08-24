ee4d4ce3c6fb45c6e1ba6a05c36d5d5943796a65
JaySzn992000 <121809806+JaySzn992000@users.noreply.github.com>
2025-08-24 21:17:14 +0530

    Update LicoricePowder.js

--- src/Products/LicoricePowder.js
+++ src/Products/LicoricePowder.js
@@ -195,10 +195,7 @@ loading="lazy"
 </section>
 </div>
 
-<div className="header_Filter">
-{" "}
-{showFilters && <Filterheader></Filterheader>}{" "}
-</div>
+<Filterheader></Filterheader>
 
 </div>
 
